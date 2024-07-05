ca48d8df7ee49677987d07f070b6950ce4350de7
Yvan Norsa <yvan.norsa@gmail.com>
2024-07-05 11:04:11 +0200

    initial import

---
+++ src/test/java/com/luxqualityconsulting/seleniumintro/SampleTest.java
@@ -0,0 +1,38 @@
+package com.luxqualityconsulting.seleniumintro;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SampleTest
+{
+    @Test
+    public void runSelenium() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        assertEquals("Web form", driver.getTitle());
+
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+        textBox.sendKeys("Selenium");
+
+        Thread.sleep(2000);
+
+        submitButton.click();
+
+        Thread.sleep(2000);
+
+        WebElement message = driver.findElement(By.id("message"));
+        assertEquals("Received!", message.getText());
+
+        driver.quit();
+    }
+}
