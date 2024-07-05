dd36a5e1b4afb29719a7c5febaa5787a48917f29
Yvan Norsa <yvan.norsa@gmail.com>
2024-07-05 11:28:32 +0200

    added headless mode

--- src/test/java/com/luxqualityconsulting/seleniumintro/SampleTest.java
+++ src/test/java/com/luxqualityconsulting/seleniumintro/SampleTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +14,13 @@ public class SampleTest
 {
     @Test
     public void runSelenium() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
+        if (System.getProperty("headless") != null) {
+            options.addArguments("--headless=new");
+        }
+
+        WebDriver driver = new ChromeDriver(options);
 
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
